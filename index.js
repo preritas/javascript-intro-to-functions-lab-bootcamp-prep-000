c616c6170dff70d951f7f340fe20802d1673c5b2
s.prerita@gmail.com <s.prerita@gmail.com>
2018-09-13 18:39:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log('HELLO')}
+<<<<<<< HEAD
 function logWhisper(string) {console.log('hello')}
 function sayHiToGrandma(string) {return `$('HELLO'.toLowerCase()}`
 
 
 
 
+=======
+>>>>>>> f4824974d6fb4ed04b36a3e7462c512021eff69b
